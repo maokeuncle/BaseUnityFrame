049e3b7ea64d6c5f0c50629aa8581948d4125e8d
hlwy_yzh <yuanzhh@hlwymobi.com>
2020-10-13 11:28:50 +0800

    自定义事件系统

---
+++ BaseFameWork/Assets/Scripts/Event/CustomEventManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class CustomEventManager
+{
+
+    public delegate void eventHandler(CusEventArgs e);
+
+
+    private static Dictionary<string, HashSet<eventHandler>> eventDic = new Dictionary<string, HashSet<eventHandler>>();
+    
+
+    public static void AddListener(string type,eventHandler listener)
+    {
+        if (!eventDic.ContainsKey(type))
+        {
+            HashSet<eventHandler> eventCollection = new HashSet<eventHandler>();
+            eventDic.Add(type,eventCollection);
+            eventDic[type].Add(listener);
+        }
+        else
+        {
+            eventDic[type].Add(listener);
+        }
+    }
+
+
+    public static void DispatchEvent(CusEventArgs e)
+    {
+        foreach (var item in eventDic[e.type])
+        {
+            item?.Invoke(e);
+        }
+    }
+
+    public static void RemoveListener(string type, eventHandler listener)
+    {
+        if(!eventDic.ContainsKey(type))
+            Debug.LogError(type+":不存在！");
+        else
+        {
+            eventDic[type].Remove(listener);
+        }
+    }
+    public static void RemoveAllListeners(string type)
+    {
+        if(!eventDic.ContainsKey(type))
+            Debug.LogError(type+":不存在！");
+        else
+        {
+            eventDic[type].Clear();
+        }
+    }
+    
+}
+
+public class CusEventArgs
+{
+    public readonly string type;
+    public readonly object message;
+    public readonly object[] messageArr;
+    
+    public CusEventArgs(string type)
+    {
+        this.type = type;
+    }
+
+    public CusEventArgs(string type, params object[] messageArr)
+    {
+        this.type = type;
+        this.messageArr = messageArr;
+    }
+    public CusEventArgs(string type, object message)
+    {
+        this.type = type;
+        this.message = message;
+    }
+}
---
+++ BaseFameWork/Assets/Scripts/Event/EventDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CustomEventManager.AddListener("OnPress_A",OnPressA);
+        CustomEventManager.AddListener("OnPress_A",OnPressA1);
+        CustomEventManager.AddListener("OnPress_A",OnPressA2);
+        CustomEventManager.RemoveListener("OnPress_A",OnPressA1);
+    }
+
+    private void OnPressA2(CusEventArgs e)
+    {
+        /***********
+         Do Something
+         ************/
+        print("第三个监听者执行了");
+    }
+    private void OnPressA1(CusEventArgs e)
+    {
+        print("第二个监听者执行了");
+    }
+
+    private void OnPressA(CusEventArgs e)
+    {
+        print("第一个监听者执行了");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+            CustomEventManager.DispatchEvent(new CusEventArgs("OnPress_A"));
+    }
+}
